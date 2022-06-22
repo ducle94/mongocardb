9e5d86020b819ee0a5c00026d6b5702127383172
Duc Le <hieuduc.le94@gmail.com>
2022-06-22 18:07:23 +0300

    upload code

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require("./routes")
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/', routes);
+
+//MongoDB connection
+const mongoose = require('mongoose');
+const mongoURL = 'mongodb+srv://mongoviope:Viope123@cluster0.7th0qt4.mongodb.net/cardb?retryWrites=true&w=majority';
+mongoose.connect(mongoURL, { useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
